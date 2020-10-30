7ec12aae3d36664a6f86c6c23345ad886407c189
Diogo Correia <me@diogotc.com>
2020-10-30 09:44:27 +0000

    Add backend Hello World code
    Rewrite some package.json files.
    Updated required node version to v14.15.0 (Latest LTS)
    
    Signed-off-by: Diogo Correia <me@diogotc.com>

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (_req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
