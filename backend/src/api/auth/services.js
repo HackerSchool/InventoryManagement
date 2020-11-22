9c1f351b302d1e0538d61f937b364302fda7903f
H-Pocinho <hapocinho@gmail.com>
2020-11-22 19:16:54 +0000

    Corrected some more mistakes (closes #12)

--- backend/src/api/auth/services.js
+++ backend/src/api/auth/services.js
@@ -1,9 +1,9 @@
 const JWT_SECRET = process.env.JWT_SECRET || 'hackerschool-is-cool';
 const { sign } = require('jsonwebtoken');
 
-function JWTGenerator(id, istId, name, role) {
+function generateJWT(id, istId, name, role) {
   const tokenPayload = { id, istId, name, role };
   const token = sign(tokenPayload, JWT_SECRET, { expiresIn: 60 * 60 * 24 * 7 });
   return token;
 }
-module.exports = { JWTGenerator }
+module.exports = { generateJWT }
