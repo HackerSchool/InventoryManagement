54fe9c517e7f29537ae8f9003b67b6f31144adff
H-Pocinho <hapocinho@gmail.com>
2020-11-30 23:09:22 +0000

    Add POST `/location` create endpoint (closes #34)

--- backend/src/api/locations/routes.js
+++ backend/src/api/locations/routes.js
@@ -2,29 +2,36 @@ const controller = require('./controller');
 
 module.exports = {
   findAll: async (req, res) => {
-    if (!req.user?.hasPermission('user')) {
-      res.sendStatus(401);
-      return;
-    }
+    if (!req.user?.hasPermission('user')) return res.sendStatus(401);
 
     res.json(await controller.findAll(req.db));
   },
 
   findOne: async (req, res) => {
+    if (!req.user?.hasPermission('user')) return res.sendStatus(401);
+
     const { id } = req.params;
     const location = await controller.findOne(req.db, id);
     if (!location) return res.sendStatus(404);
     res.json(location);
   },
 
-  create: async (req, res) => {},
+  create: async (req, res) => {
+    if (!req.user?.hasPermission('admin')) return res.sendStatus(401);
+    const data = req.body;
+    res.json(await controller.create(req.db, data));
+  },
 
   remove: async (req, res) => {
+    if (!req.user?.hasPermission('admin')) return res.sendStatus(401);
+
     const { id } = req.params;
     // TODO
   },
 
   update: async (req, res) => {
+    if (!req.user?.hasPermission('admin')) return res.sendStatus(401);
+
     const { id } = req.params;
     // TODO
   },
