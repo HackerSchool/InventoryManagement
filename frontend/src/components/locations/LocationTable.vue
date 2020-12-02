1caf7d9badf17ea2ba528fab0c8d6d0923fb9ccc
Diogo Correia <me@diogotc.com>
2020-12-02 23:05:00 +0000

    Start linking locations table with Vuex store
    Signed-off-by: Diogo Correia <me@diogotc.com>

---
+++ frontend/src/components/locations/LocationTable.vue
@@ -0,0 +1,146 @@
+<template>
+  <v-data-table :headers="headers" :items="locations" sort-by="name" class="elevation-1">
+    <template #top>
+      <v-toolbar flat>
+        <v-toolbar-title>Locations</v-toolbar-title>
+        <v-divider class="mx-4" inset vertical></v-divider>
+        <v-spacer></v-spacer>
+        <v-dialog v-model="dialog" max-width="600px">
+          <template #activator="{ on, attrs }">
+            <v-btn color="secondary" dark class="mb-2" v-bind="attrs" v-on="on"> New Item </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="headline">{{ formTitle }}</span>
+            </v-card-title>
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.name" label="Location"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="8" md="8">
+                    <v-textarea
+                      v-model="editedItem.description"
+                      label="description"
+                      filled
+                    ></v-textarea>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="close"> Cancel </v-btn>
+              <v-btn color="blue darken-1" text @click="save"> Save </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template #item.actions="{ item }">
+      <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
+      <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex';
+
+export default {
+  data: () => ({
+    dialog: false,
+    dialogDelete: false,
+    headers: [
+      { text: 'Location', value: 'name' },
+      { text: 'Description', value: 'description', sortable: false },
+      { text: 'Actions', value: 'actions', sortable: false },
+    ],
+    editedIndex: -1,
+    editedItem: {
+      name: '',
+      description: '',
+    },
+    defaultItem: {
+      name: '',
+      description: '',
+    },
+  }),
+
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? 'New Item' : 'Edit Item';
+    },
+    ...mapState('locations', ['locations']),
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.close();
+    },
+    dialogDelete(val) {
+      val || this.closeDelete();
+    },
+  },
+
+  methods: {
+    editItem(item) {
+      this.editedIndex = this.locations.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+
+    deleteItem(item) {
+      this.editedIndex = this.locations.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialogDelete = true;
+    },
+
+    deleteItemConfirm() {
+      // TODO call this.deleteLocation(id)
+      this.locations.splice(this.editedIndex, 1);
+      this.closeDelete();
+    },
+
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    closeDelete() {
+      this.dialogDelete = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        // TODO call this.updateLocation({id, data})
+        Object.assign(this.locations[this.editedIndex], this.editedItem);
+      } else {
+        this.createLocation(this.editedItem);
+      }
+      this.close();
+    },
+
+    ...mapActions('locations', ['updateLocation', 'createLocation', 'deleteLocation']),
+  },
+};
+</script>
