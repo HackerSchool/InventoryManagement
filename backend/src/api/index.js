585aad7272eb5879f33ff3162f24eb21990d6b53
H-Pocinho <hapocinho@gmail.com>
2020-12-03 11:33:12 +0000

    Add './materials' to index

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -1,6 +1,7 @@
 const auth = require('./auth');
 const locations = require('./locations');
 const members = require('./members');
+const materials = require('./materials');
 
 module.exports = {
   // Add all routes to express 'app'
@@ -8,5 +9,6 @@ module.exports = {
     auth.init(app);
     locations.init(app);
     members.init(app);
+    materials.init(app);
   },
 };
