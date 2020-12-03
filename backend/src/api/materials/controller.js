d0a5c40974328c48b7f33e86f7b32efb654cf2dd
H-Pocinho <hapocinho@gmail.com>
2020-12-03 15:09:28 +0000

    Add endpoint material functions

--- backend/src/api/materials/controller.js
+++ backend/src/api/materials/controller.js
@@ -1,8 +1,19 @@
 module.exports = {
   async findAll(database) {
     const result = await database
-      .select('id', 'name', 'description', 'stock', 'state', 'type', 'location_id')
-      .from('materials');
+      .select(
+        'id',
+        'name',
+        'description',
+        'stock',
+        'state',
+        'type',
+        { locationId: 'location_id' },
+        { locationName: 'locations.name' },
+        { locationDesc: 'locations.description' }
+      )
+      .from('materials')
+      .leftJoin('locations', 'material.location_id', 'locations.id');
 
     return result.map((material) => ({
       id: material.id,
@@ -17,7 +28,7 @@ module.exports = {
 
   async findOne(database, id) {
     const result = await database
-      .select('id', 'name', 'description')
+      .select('id', 'name', 'description', 'stock', 'state', 'type', 'location_id')
       .where('id', id)
       .from('locations');
 
@@ -25,20 +36,22 @@ module.exports = {
     return result[0];
   },
 
-  async create(database, { name, description }) {
-    const result = await database.insert({ name, description }).into('locations');
+  async create(database, { name, description, stock, state, type, location_id }) {
+    const result = await database
+      .insert({ name, description, stock, state, type, location_id })
+      .into('materials');
 
     // Knex returns the inserted id, so we get the object from the database.
     return this.findOne(database, result[0]);
   },
 
   async remove(database, id) {
-    const affectedRows = await database.where('id', id).from('locations').delete();
+    const affectedRows = await database.where('id', id).from('materials').delete();
     return affectedRows > 0;
   },
 
   async update(database, id, data) {
-    const affectedRows = await database('locations').where('id', id).update(data);
+    const affectedRows = await database('materials').where('id', id).update(data);
     if (affectedRows > 0) return this.findOne(database, id);
     // else return undefined
   },
