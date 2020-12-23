7a30b31947e22ea055985d6ec5274993f5510e51
H-Pocinho <hapocinho@gmail.com>
2020-12-23 13:07:26 +0000

    Add endpoint to list all requisitions (closes #49)

---
+++ backend/src/api/requisitions/controller.js
@@ -0,0 +1,33 @@
+const fields = [
+  'requisitions.id',
+  'requisitions.quantity',
+  'requisitions.created_at',
+  'requisitions.date_out',
+  'requisitions.date_in',
+  { memberId: 'member_id' },
+  { memberName: 'members.name' },
+  { memberIstId: 'members.ist_id' },
+  { materialId: 'material_id' },
+  { materialName: 'materials.name' },
+];
+
+const formatResponse = (response) => ({
+  id: response.id,
+  quantity: response.quantity,
+  created_at: response.created_at,
+  date_in: response.date_in,
+  date_out: response.date_out,
+  material: { id: response.materialId, name: response.materialName },
+  member: { id: response.memberId, istId: response.memberIstId, name: response.memberName },
+});
+
+module.exports = {
+  async findAll(database) {
+    const result = await database
+      .select(...fields)
+      .from('requisitions')
+      .leftJoin('materials', 'requistions.id_materials', 'materials.id')
+      .leftJoin('members', 'requisitions.id_members', 'members.id');
+    return result.map(formatResponse);
+  },
+};
