f06c7fb2ee4f663ee9ea663bc03dcd431a345d96
Diogo Correia <me@diogotc.com>
2021-01-03 11:57:02 +0000

    Add 'state' column to 'requisitions' table
    Signed-off-by: Diogo Correia <me@diogotc.com>

---
+++ backend/migrations/20210103_111700_requisitions_state.js
@@ -0,0 +1,22 @@
+/*
+This migration will add a 'state' field to the 'requisitions' table.
+It will also make 'id_material' and 'id_member' non-nullable.
+*/
+
+module.exports.up = async (knex) => {
+  await knex.schema.alterTable('requisitions', (table) => {
+    table.integer('id_member').unsigned().notNullable().alter();
+    table.integer('id_material').unsigned().notNullable().alter();
+
+    // enum: "pending", "can_pickup", "active", "returned", "not_returning"
+    table.string('state', 25).notNullable().default('pending');
+  });
+};
+
+module.exports.down = async (knex) => {
+  await knex.schema.alterTable('requisitions', (table) => {
+    table.dropColumn('state');
+    table.integer('id_material').unsigned().nullable().alter();
+    table.integer('id_member').unsigned().nullable().alter();
+  });
+};
