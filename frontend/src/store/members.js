3c422d3f7c7ef21e5d4f23ddf9789574a9d124b1
Diogo Correia <me@diogotc.com>
2021-01-03 00:04:21 +0000

    Finish linking the members table with the API
    Signed-off-by: Diogo Correia <me@diogotc.com>

--- frontend/src/store/members.js
+++ frontend/src/store/members.js
@@ -1,22 +1,53 @@
 // import the api endpoints
-import { getAllMembers } from '@/api/members.api';
+import * as memberApi from '@/api/members.api';
 
 const state = {
   members: [],
 };
 
-const getters = {};
+const getters = {
+  getMember(id) {
+    return state.members.find((member) => member.id == id);
+  },
+};
 
 const actions = {
   async fetchMembers({ commit }) {
-    const response = await getAllMembers();
+    const response = await memberApi.getAllMembers();
     commit('SET_MEMBERS', response.data);
+  },
+  async fetchMember({ commit }, id) {
+    const response = await memberApi.getMember(id);
+    commit('SET_MEMBER', response.data);
+  },
+  async createMember({ commit }, data) {
+    const response = await memberApi.addMember(data);
+    commit('SET_MEMBER', response.data);
+  },
+  async deleteMember({ commit }, id) {
+    await memberApi.deleteMember(id);
+    commit('REMOVE_MEMBER', id);
+  },
+  async updateMember({ commit }, { id, data }) {
+    const response = await memberApi.updateMember(id, data);
+    commit('SET_MEMBER', { ...response.data, id });
   },
 };
 
 const mutations = {
   SET_MEMBERS(state, data) {
     state.members = data;
+  },
+  SET_MEMBER(state, data) {
+    const index = state.members.findIndex((member) => member.id == data.id);
+
+    if (index === -1) state.members.push(data);
+    // editing the array doesn't trigger DOM refresh, but splice does
+    else state.members.splice(index, 1, data);
+  },
+  REMOVE_MEMBER(state, id) {
+    const index = state.members.findIndex((member) => member.id == id);
+    state.members.splice(index, 1); // removes element from array and shifts the others
   },
 };
 
