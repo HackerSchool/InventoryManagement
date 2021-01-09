f326304041c2df50391044c8e9ba97705e52d073
Diogo Correia <me@diogotc.com>
2021-01-09 00:11:33 +0000

    Add PWA support
    Signed-off-by: Diogo Correia <me@diogotc.com>

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,37 @@ process.env.VUE_APP_VERSION = process.env.npm_package_version || 'unknown';
 
 module.exports = {
   publicPath: '/',
+  pwa: {
+    name: 'Hackerschool Inventory',
+    manifestOptions: {
+      short_name: 'HS Inventory',
+      background_color: '#2B2A29',
+      icons: [
+        {
+          src: '/img/icons/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/img/icons/icon-256x256.png',
+          sizes: '256x256',
+          type: 'image/png',
+        },
+        {
+          src: '/img/icons/icon-384x384.png',
+          sizes: '384x384',
+          type: 'image/png',
+        },
+        {
+          src: '/img/icons/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
+    themeColor: '#2B2A29',
+    msTileColor: '#6dba83',
+  },
   runtimeCompiler: true,
   devServer: {
     proxy: {
